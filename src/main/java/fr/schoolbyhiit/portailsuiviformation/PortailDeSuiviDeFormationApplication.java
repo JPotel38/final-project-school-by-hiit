972ec0675ff552852862db3dfbc8321ed844202c
Lem Benjamin <benjamin.lem.ext@inetum.world>
2021-01-22 11:41:42 +0100

    Spring initializr

---
+++ src/main/java/fr/schoolbyhiit/portailsuiviformation/PortailDeSuiviDeFormationApplication.java
@@ -0,0 +1,13 @@
+package fr.schoolbyhiit.portailsuiviformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailDeSuiviDeFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailDeSuiviDeFormationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/schoolbyhiit/portailsuiviformation/PortailDeSuiviDeFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.schoolbyhiit.portailsuiviformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailDeSuiviDeFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
