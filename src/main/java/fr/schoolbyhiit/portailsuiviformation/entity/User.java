1d5cdca8ce534a4d84fa6b7c44adbb66a30e7f9f
Mathieu V <mathieu.verpillat@gmail.com>
2021-03-09 15:37:58 +0100

    improve user constaints

--- src/main/java/fr/schoolbyhiit/portailsuiviformation/entity/User.java
+++ src/main/java/fr/schoolbyhiit/portailsuiviformation/entity/User.java
@@ -13,6 +13,9 @@ import java.util.Set;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 
 @Entity
 @Table(name="users")
@@ -29,18 +32,24 @@ public class User {
     private Long id;
 
     @Column(name = "first_name")
+    @NotBlank
     private String firstName;
 
     @Column(name = "last_name")
+    @NotBlank
     private String lastName;
 
     @Column(name = "creation_date")
+    @NotNull
     private LocalDate creationDate;
 
     @Column(name = "birth_date")
+    @NotNull
+    @Past
     private LocalDate birthDate;
 
     @Column(name = "mail", unique = true, nullable = false)
+    @NotBlank
     @Email
     private String mail;
 
