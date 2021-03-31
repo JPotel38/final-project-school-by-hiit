c00dac9fdc8685169e9662c2ee2e615860f7bff8
Mathieu Verpillat <mathieu.verpillat@gmail.com>
2021-03-31 10:08:40 +0200

    fix: retours PR

--- src/main/java/fr/schoolbyhiit/portailsuiviformation/service/impl/UserServiceImpl.java
+++ src/main/java/fr/schoolbyhiit/portailsuiviformation/service/impl/UserServiceImpl.java
@@ -2,32 +2,26 @@ package fr.schoolbyhiit.portailsuiviformation.service.impl;
 
 import fr.schoolbyhiit.portailsuiviformation.controller.exception.EmailExistsException;
 import fr.schoolbyhiit.portailsuiviformation.controller.exception.UserNotFoundException;
-import fr.schoolbyhiit.portailsuiviformation.dao.RoleRepository;
 import fr.schoolbyhiit.portailsuiviformation.dao.UserRepository;
 import fr.schoolbyhiit.portailsuiviformation.dto.UserDto;
-import fr.schoolbyhiit.portailsuiviformation.entity.Role;
 import fr.schoolbyhiit.portailsuiviformation.entity.User;
-import fr.schoolbyhiit.portailsuiviformation.entity.model.RoleName;
 import fr.schoolbyhiit.portailsuiviformation.mapper.UserMapper;
 import fr.schoolbyhiit.portailsuiviformation.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
-import java.util.Set;
 
 @Service
-@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
 
-
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 
     @Override
     public UserDto create(UserDto userDto) {
