9e47c5358a15fba0693dd8344c50eeba28c52a94
thibault <mathieu.verpillat@hiit-consulting.fr>
2021-06-18 15:10:29 +0200

    feat: lockback.xml

--- src/main/java/fr/schoolbyhiit/portailsuiviformation/PortailDeSuiviDeFormationApplication.java
+++ src/main/java/fr/schoolbyhiit/portailsuiviformation/PortailDeSuiviDeFormationApplication.java
@@ -1,5 +1,7 @@
 package fr.schoolbyhiit.portailsuiviformation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
@@ -9,5 +11,6 @@ public class PortailDeSuiviDeFormationApplication {
 
     public static void main(String[] args) {
             SpringApplication.run(PortailDeSuiviDeFormationApplication.class, args);
+
     }
 }
