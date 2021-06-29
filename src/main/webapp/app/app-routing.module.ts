c5c489316f415250281031a8954d2b03e62f669b
Jérémy <potel.jeremy@gmail.com>
2021-06-29 13:26:04 +0200

    rebase

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -39,6 +39,7 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
 ];
 
 @NgModule({
--- src/main/webapp/app/home/home.page.ts
+++ src/main/webapp/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UsersService} from "../shared/users.service";
+import {User} from "../shared/interfaces/user/user";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy{
+  userSubscription: Subscription;
 
-  constructor() {}
+  constructor(public usersService : UsersService) {}
 
+  ngOnInit() {
+   this.userSubscription = this.usersService.getUsersFromBack().subscribe((data: User[]) =>
+  console.log(data))
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
 }
