83c4489e3068491eee49ed5cd46c48eaea722e58
Michaël COLL <michael.coll@hiit-consulting.fr>
2021-07-05 21:52:56 +0200

    test: Correction TU.

--- src/test/java/fr/schoolbyhiit/portailsuiviformation/service/impl/UserServiceImplTest.java
+++ src/test/java/fr/schoolbyhiit/portailsuiviformation/service/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package fr.schoolbyhiit.portailsuiviformation.service.impl;
 
+import fr.schoolbyhiit.portailsuiviformation.dao.RoleRepository;
 import fr.schoolbyhiit.portailsuiviformation.dao.UserRepository;
 import fr.schoolbyhiit.portailsuiviformation.dto.UserDto;
 import fr.schoolbyhiit.portailsuiviformation.entity.Role;
@@ -34,28 +35,29 @@ class UserServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
-
+    @Mock
+    private RoleRepository roleRepository;
     @Mock
     private UserMapperImpl userMapper;
 
     @InjectMocks
     private UserServiceImpl userService;
 
-
     @Test
     void create() {
         //GIVEN
         User user = initUser();
         UserDto userDto = UserDto.builder()
-                .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
-                .birthDate(BIRTH_DATE)
-                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                .build();
+            .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
+            .birthDate(BIRTH_DATE)
+            .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+            .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+            .build();
         when(userMapper.toUserDto(any(User.class))).thenCallRealMethod();
         when(userMapper.toUser(any(UserDto.class))).thenCallRealMethod();
         when(userRepository.getUserByMail(anyString())).thenReturn(Optional.empty());
         when(userRepository.save(any(User.class))).thenReturn(user);
+        when(roleRepository.findByName(RoleName.ADMIN)).thenReturn(Optional.of(Role.builder().name(RoleName.ADMIN).build()));
         //WHEN
         UserDto actual = userService.create(userDto);
         //THEN
@@ -70,17 +72,16 @@ class UserServiceImplTest {
         //GIVEN
         User user = initUser();
         UserDto userDto = UserDto.builder()
-                .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
-                .birthDate(BIRTH_DATE)
-                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                .build();
+            .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
+            .birthDate(BIRTH_DATE)
+            .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+            .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+            .build();
         when(userRepository.getUserByMail(anyString())).thenReturn(Optional.of(user));
         //WHEN //THEN
         assertThatThrownBy(() -> userService.create(userDto))
-                .isInstanceOf(EmailExistsException.class)
-                .hasFieldOrPropertyWithValue("reason", "User with mail : " + userDto.getMail() + " already exist !");
-
+            .isInstanceOf(EmailExistsException.class)
+            .hasFieldOrPropertyWithValue("reason", "User with mail : " + userDto.getMail() + " already exist !");
     }
 
     @Test
@@ -103,9 +104,8 @@ class UserServiceImplTest {
         when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
         //WHEN THEN
         assertThatThrownBy(() -> userService.findById(userId))
-                .isInstanceOf(UserNotFoundException.class)
-                .hasFieldOrPropertyWithValue("reason", "User with id : " + userId + " not found !");
-
+            .isInstanceOf(UserNotFoundException.class)
+            .hasFieldOrPropertyWithValue("reason", "User with id : " + userId + " not found !");
     }
 
     @Test
@@ -119,13 +119,12 @@ class UserServiceImplTest {
         List<UserDto> actual = userService.findAll();
         //THEN
         assertThat(actual)
-                .isNotNull()
-                .isNotEmpty()
-                .hasSameSizeAs(users);
+            .isNotNull()
+            .isNotEmpty()
+            .hasSameSizeAs(users);
         assertThat(actual.get(1))
-                .usingRecursiveComparison()
-                .isEqualTo(users.get(1));
-
+            .usingRecursiveComparison()
+            .isEqualTo(users.get(1));
     }
 
     @Test
@@ -136,17 +135,19 @@ class UserServiceImplTest {
         when(userMapper.toUserDto(any(User.class))).thenCallRealMethod();
         when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
         when(userRepository.save(any(User.class))).thenReturn(user);
+        when(roleRepository.findByName(RoleName.ADMIN)).thenReturn(Optional.of(Role.builder().name(RoleName.ADMIN).build()));
         UserDto updatedUser = UserDto.builder()
-                .id(1L).firstName("new firstname").lastName("new lastname")
-                .birthDate(BIRTH_DATE)
-                .mail(MAIL).phoneNumber(PHONE_NUMBER)
-                .build();
+            .id(1L).firstName("new firstname").lastName("new lastname")
+            .birthDate(BIRTH_DATE)
+            .mail(MAIL).phoneNumber(PHONE_NUMBER)
+            .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+            .build();
         //WHEN
         UserDto actual = userService.update(userId, updatedUser);
         //THEN
         assertThat(actual)
-                .hasFieldOrPropertyWithValue("firstName", "new firstname")
-                .hasFieldOrPropertyWithValue("lastName", "new lastname");
+            .hasFieldOrPropertyWithValue("firstName", "new firstname")
+            .hasFieldOrPropertyWithValue("lastName", "new lastname");
         verify(userRepository, times(1)).save(any(User.class));
     }
 
@@ -156,14 +157,14 @@ class UserServiceImplTest {
         Long userId = 1L;
         when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
         UserDto updatedUser = UserDto.builder()
-                .id(1L).firstName("new firstname").lastName("new lastname")
-                .birthDate(BIRTH_DATE)
-                .mail(MAIL).phoneNumber(PHONE_NUMBER)
-                .build();
+            .id(1L).firstName("new firstname").lastName("new lastname")
+            .birthDate(BIRTH_DATE)
+            .mail(MAIL).phoneNumber(PHONE_NUMBER)
+            .build();
         //WHEN //THEN
         assertThatThrownBy(() -> userService.update(userId, updatedUser))
-                .isInstanceOf(UserNotFoundException.class)
-                .hasFieldOrPropertyWithValue("reason", "User with id : " + userId + " not found !");
+            .isInstanceOf(UserNotFoundException.class)
+            .hasFieldOrPropertyWithValue("reason", "User with id : " + userId + " not found !");
     }
 
     @Test
@@ -182,10 +183,9 @@ class UserServiceImplTest {
         Long userId = 1L;
         //WHEN //THEN
         assertThatThrownBy(() -> userService.delete(userId))
-                .isInstanceOf(UserNotFoundException.class)
-                .hasFieldOrPropertyWithValue("reason", "User with id : " + userId + " not found !");
+            .isInstanceOf(UserNotFoundException.class)
+            .hasFieldOrPropertyWithValue("reason", "User with id : " + userId + " not found !");
     }
-
 
     private static final String FIRSTNAME = "firstname";
     private static final String LASTNAME = "lastname";
@@ -194,54 +194,53 @@ class UserServiceImplTest {
     private static final String PHONE_NUMBER = "00000000";
     private static final LocalDate BIRTH_DATE = LocalDate.of(2001, 1, 1);
 
-
     private User initUser() {
         return User.builder()
-                .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
-                .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
-                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                .build();
+            .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
+            .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
+            .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+            .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+            .build();
     }
 
     private UserDto initUserDto() {
         return UserDto.builder()
-                .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
-                .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
-                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                .build();
+            .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
+            .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
+            .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+            .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+            .build();
     }
 
     private List<User> initSomeUsers() {
         return List.of(
-                User.builder()
-                        .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
-                        .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
-                        .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                        .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                        .build(),
-                User.builder()
-                        .id(2L).firstName(FIRSTNAME).lastName(LASTNAME)
-                        .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
-                        .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                        .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                        .build());
+            User.builder()
+                .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
+                .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
+                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+                .build(),
+            User.builder()
+                .id(2L).firstName(FIRSTNAME).lastName(LASTNAME)
+                .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
+                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+                .build());
     }
 
     private List<UserDto> initSomeUserDtoList() {
         return List.of(
-                UserDto.builder()
-                        .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
-                        .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
-                        .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                        .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                        .build(),
-                UserDto.builder()
-                        .id(2L).firstName(FIRSTNAME).lastName(LASTNAME)
-                        .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
-                        .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
-                        .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
-                        .build());
+            UserDto.builder()
+                .id(1L).firstName(FIRSTNAME).lastName(LASTNAME)
+                .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
+                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+                .build(),
+            UserDto.builder()
+                .id(2L).firstName(FIRSTNAME).lastName(LASTNAME)
+                .birthDate(BIRTH_DATE).creationDate(LocalDate.now())
+                .mail(MAIL).password(PASSWORD).phoneNumber(PHONE_NUMBER)
+                .roles(Set.of(Role.builder().name(RoleName.ADMIN).build()))
+                .build());
     }
 }
