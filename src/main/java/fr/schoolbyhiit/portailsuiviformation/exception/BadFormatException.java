e79fd314fc3e8f9d0f5568fb3a7f151e02a161c1
eva_delpierre <eva_delpierre@carrefour.com>
2021-07-05 14:05:59 +0200

    maj TU projects sur Develop

--- src/main/java/fr/schoolbyhiit/portailsuiviformation/exception/BadFormatException.java
+++ src/main/java/fr/schoolbyhiit/portailsuiviformation/exception/BadFormatException.java
@@ -3,7 +3,7 @@ package fr.schoolbyhiit.portailsuiviformation.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Le format des donnés est incorrect" )
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Le format des données est incorrect" )
 public class BadFormatException extends RuntimeException {
     public BadFormatException(String s){
         super(s);
