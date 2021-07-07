8ec6ff55cf7efdb7b5ec6110c8c2eff735410d1a
eva_delpierre <eva_delpierre@carrefour.com>
2021-07-07 10:40:45 +0200

    maj home page

--- src/main/webapp/app/home/home.page.ts
+++ src/main/webapp/app/home/home.page.ts
@@ -7,18 +7,13 @@ import {Subscription} from "rxjs";
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit, OnDestroy{
-  userSubscription: Subscription;
-
   constructor(
-//   public usersService : UsersService
   ) {}
 
   ngOnInit() {
-//    this.userSubscription = this.usersService.getUsersFromBack().subscribe((data: User[]) =>
-//   console.log(data))
   }
 
+
   ngOnDestroy() {
-    this.userSubscription.unsubscribe();
   }
 }
